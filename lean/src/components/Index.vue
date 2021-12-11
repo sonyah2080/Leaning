3191dc9576ac73013c705750bc5108dd10c80e43
ili <ili@iliui-MacBookPro.local>
2021-12-11 18:51:22 +0900

    add Vue file

---
+++ lean/src/components/Index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <table>
+      <thead>
+        <tr>
+          <th>No.1</th>
+          <th>No.2</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th>1</th>
+          <td>Yes</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<style>
+  table {
+    th { padding: 1rem; }
+  }
+</style>
